6a8ae3aaddb25ff3120fa458fa536a153f804a6c
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2026-01-26 18:47:57 +0530

    moved smart finance manager into new repo

--- smart-finance-manager/src/config/AppConfig.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public class AppConfig{
-    public static final String DATA_FILE = "data/transactions.csv";
-    public static final String FULL_DATE_FORMAT = "d-M-yyyy";
-    public static final String HALF_DATE_FORMAT = "M-uuuu";
-}
